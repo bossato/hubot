37fed08f4bbf522619776a3550645e338785c83a
Kazuya Sato <recoba0110@gmail.com>
2016-01-29 10:00:10 +0900

    Added netatmo script

--- bin/chatwork.sh
+++ bin/chatwork.sh
@@ -4,4 +4,10 @@ export HUBOT_CHATWORK_TOKEN=xxxxxx
 export HUBOT_CHATWORK_ROOMS=xxxxxx
 export HUBOT_CHATWORK_API_RATE=800
 
-hubot -a chatwork -n bot_name
+export NETATMO_CILIENT_ID=""
+export NETATMO_CILIENT_SECRET=""
+export NETATMO_USERNAME=""
+export NETATMO_PASSWORD=""
+export NETATMO_DEVICE_ID=""
+
+hubot -a chatwork -n %bot_name%
